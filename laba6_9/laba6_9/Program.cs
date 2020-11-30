03572693f1a161bdf272a689dd6f1a5b2419fac1
EGOR <70574235+egor2kurs@users.noreply.github.com>
2020-11-30 22:29:55 +0300

    Add files via upload

---
+++ laba6_9/laba6_9/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+namespace laba6_9
+{
+    interface IMyInterface
+    {
+        string MyFunc();
+    }
+
+    interface IAnotherInterface
+    {
+        string ToString();
+    }
+
+    partial class Product : Base, IAnotherInterface
+    {
+        public string OrganizationName { get; set; }
+        public string ProductName { get; set; }
+
+        public Product()
+        {
+            OrganizationName = "NoName organization";
+            ProductName = "NoName product";
+        }
+
+        public Product(string organization, string product, int price, int mas)
+        {
+            OrganizationName = organization;
+            ProductName = product;
+            info.price = price;
+            info.mas = mas;
+        }
+
+
+    }
+
+    class Sweets : Product, IAnotherInterface
+    {
+        public string SweetsType { get; set; }
+        public Sweets() : base()
+        {
+            SweetsType = "NoName SweetsType";
+        }
+
+        public Sweets(string organization, string product, string sweetstype, int price, int mas) : base(organization, product, price, mas)
+        {
+            SweetsType = sweetstype;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.SweetsType;
+        }
+    }
+
+    abstract class Cakes : Sweets, IAnotherInterface
+    {
+        public string Cream { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.Cream;
+        }
+
+        public Cakes() : base()
+        {
+            Cream = "NoName cream";
+        }
+
+        public Cakes(string organization, string product, string SweetsType, string cream, int price, int mas) : base(organization, product, SweetsType, price, mas)
+        {
+            Cream = cream;
+        }
+
+        public abstract string MyFunc();
+    }
+
+    sealed class Candys : Sweets, IAnotherInterface
+    {
+        public string Filling { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.Filling;
+        }
+
+        public Candys() : base()
+        {
+            Filling = "NoName Filling";
+        }
+
+        public Candys(string organization, string product, string manufacturer, string inc, int price, int mas) : base(organization, product, manufacturer, price, mas)
+        {
+            Filling = inc;
+        }
+    }
+
+    class Clocks : Product, IAnotherInterface
+    {
+        public string ClockTech { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.ClockTech;
+        }
+
+        public Clocks() : base()
+        {
+            ClockTech = "NoName cTech";
+        }
+
+        public Clocks(string organization, string product, string manufacturer, string ctech, int price, int mas) : base(organization, product, price, mas)
+        {
+            ClockTech = ctech;
+        }
+    }
+
+    class Flower : Cakes, IMyInterface, IAnotherInterface
+    {
+        public string Resolution { get; set; }
+        public override string MyFunc()
+        {
+            return "Переопределение";
+        }
+        string IMyInterface.MyFunc()
+        {
+            return "Реализация метода интерфейса";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.Resolution;
+        }
+    }
+
+    class Print
+    {
+        public static void IAmPrinting(IAnotherInterface obj)  
+        {
+            Console.WriteLine(obj.ToString());
+            Console.ReadKey();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("Мороз", "продукт", 100, 500);
+            Sweets nyam = new Sweets("Мороз", "Продукт", "Тортек", 200, 1300);
+            Flower rose = new Flower();
+            Gift gift = new Gift(10);
+            rose.info.price = 300;
+            rose.info.mas = 150;
+            gift.Add(product);
+            gift.Add(nyam);
+            gift.Add(rose);
+            gift.Add(new Sweets("Мороз", "Продукт", "Буличка", 80, 400));
+            gift.Add(new Clocks("Xiaomi", "часики", "Xiaomi", "электронный", 5000, 200));
+            gift.Add(new Product("Мороз", "продукт2", 450, 400));
+
+            gift.show();
+            controller.show(gift);
+            controller.sort(gift);
+            gift.show();
+            gift.Del(rose);
+            gift.show();
+            controller.minmas(gift);
+
+
+        }
+    }
+}
---
+++ laba6_9/laba6_9/six.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba6_9
+{
+
+    partial class Product
+    {
+        public override bool Equals(object obj)
+        {
+            Product product = (Product)obj;
+            return ((this.OrganizationName == product.OrganizationName) && (this.ProductName == product.ProductName));
+        }
+
+        public override int GetHashCode()
+        {
+            return this.OrganizationName.GetHashCode() + this.ProductName.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.OrganizationName + " " + this.ProductName;
+        }
+    }
+
+    enum list//перечисление
+    {
+        one = 1,
+        two,
+        three,
+        nine = 9,
+        ten
+    }
+
+    public class Gift//контейнер
+    {
+        public Base[] elems;
+        public int count = 0;
+        public int size;
+        list list = new list();
+
+        public Gift()
+        {
+            size = 100;
+            elems = new Base[100];
+        }
+
+        public Gift(int c)
+        {
+            size = c;
+            elems = new Base[c];
+        }
+
+        public bool isFull()
+        {
+            return (count == size);
+        }
+
+        public bool isEmpty()
+        {
+            return (count == 0);
+        }
+
+        public void Add(Base el)
+        {
+            if (isFull())
+                return;
+            elems[count++] = el;
+        }
+
+        public void Del(Base el)
+
+        {
+            int num = 0;
+            if (isEmpty())
+                return;
+            for (int i = 0; i < count; i++)
+            {
+                if (elems[i] == el)
+                    num = i;
+            }
+            for (int i = num; i < count; i++)
+            {
+                elems[i] = elems[i + 1];
+            }
+            count--;
+        }
+
+        public void show()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(elems[i].ToString());
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Base
+    {
+        public struct Info//структура
+        {
+            public int price;
+            public int mas;
+        }
+        public Info info = new Info();
+    }
+
+    public static class controller//статический со статическими методами,чтобы мы смогли вызывать их ранее созданными объектами 
+    {
+        public static void sort(Gift gift)
+        {
+            Base elem;
+            for (int i = 0; i < gift.count - 1; i++)
+            {
+                for (int j = 0; j < gift.count - i - 1; j++)
+                {
+                    if (gift.elems[j].info.mas < gift.elems[j + 1].info.mas)
+                    {
+                        elem = gift.elems[j];
+                        gift.elems[j] = gift.elems[j + 1];
+                        gift.elems[j + 1] = elem;
+                    }
+                }
+            }
+        }
+
+        public static void minmas(Gift gift)
+        {
+            Base elem;
+            elem = gift.elems[0];
+            int massa = 99999;
+            for (int i = 0; i < gift.count; i++)
+            {
+                if (gift.elems[i].info.mas < massa)
+                {
+                    massa = gift.elems[i].info.mas;
+                    elem = gift.elems[i];
+
+                }
+
+            }
+            Console.Write(elem);
+            Console.WriteLine(" " + massa);
+            Console.ReadKey();
+        }
+
+        public static void show(Gift gift)
+        {
+            string[] products = new string[6];
+            int[] count = new int[6];
+            int pos = 0;
+            bool b = false;
+            for (int j = 0; j < gift.count; j++)
+            {
+                for (int i = 0; i < pos; i++)
+                {
+                    if (products[i] == (gift.elems[j].GetType()).Name)
+                    {
+                        b = true;
+                        count[i]++;
+                        break;
+                    }
+                }
+                if (!b)
+                {
+                    products[pos] = gift.elems[j].GetType().Name;
+                    count[pos]++;
+                    pos++;
+                }
+                b = false;
+            }
+
+            Console.WriteLine("Lab elements:");
+            for (int i = 0; i < pos; i++)
+            {
+                Console.WriteLine($"{products[i]} - {count[i]}");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
